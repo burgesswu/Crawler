9f801ae24376c1c10ac59c1d7f9ba1b88a1de610
Burgess <wurui_0@163.com>
2017-06-15 18:22:18 +0800

    项目构建

--- advtisement/views.py
+++ advtisement/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    context={}
+    context['hello']='burgess'
+    return render(request, 'login.html', context)
